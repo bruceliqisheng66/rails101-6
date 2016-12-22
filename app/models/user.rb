eb9dd15ba1165c133e1b2aae8ae4d1a71c448f02
li qi sheng <bruce.liqisheng66@gmail.com>
2016-12-22 16:04:28 +0800

    add post and hook group/user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :groups
+         has_many :groupsy
+         has_many :posts
 end
